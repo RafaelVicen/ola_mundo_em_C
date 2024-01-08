a1f1589f8c356ed3b453417e362a6b126a7cf71d
RafaelVicen <rafaelvic1946@gmail.com>
2024-01-08 08:30:54 +0100

    new commit

---
+++ hello_word.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+	printf("Hello, Word! \n");//primeiro ola mundo em C, esta ligada a biblioteca (stdio.h) que vem de saida e entrada de dados.
+		system("pause");//função q permiti pausar o programa no windows, esta direitamente ligada a biblioteca (stdlib.h)
+	return 0;
+	
+}
